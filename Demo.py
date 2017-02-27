d865638e96fde6e9585dd8d751fc1e7474e9f009
CaosnChen <answerczy@qq.com>
2017-02-27 21:00:53 +0800

    commit loop case

--- Demo.py
+++ Demo.py
@@ -1,15 +1,21 @@
-score =50
-if score > 90:
-    print('youxiu')
-elif score >80:
-    print('lianghao')
-elif score < 60:
-    print('bujike')
-else:
-    print('jige')
-
-
-lang = 'kr'
-greeing = {'cn':'你好','en':'hello','ru':'dada'}
-
-print(greeing.get(lang,'bucunzai'))
+# score =50
+# if score > 90:
+#     print('youxiu')
+# elif score >80:
+#     print('lianghao')
+# elif score < 60:
+#     print('bujike')
+# else:
+#     print('jige')
+#
+#
+# lang = 'kr'
+# greeing = {'cn':'你好','en':'hello','ru':'dada'}
+#
+# print(greeing.get(lang,'bucunzai'))
+num = [1,22,4,65,31,32,6,7,3,20]
+for i in range(len(num)):
+    for j in range(len(num)-i-1):
+        if num[j]>num[j+1]:
+            num[j],num[j+1] = num[j+1],num[j]
+    print(num)
--- for_in.py
+++ for_in.py
@@ -1,11 +1,24 @@
-#name = ['tom','jerry','mike']
+name = ['tom','jerry','mike']
 #name = 'www.baidu.com'
-name = ('bibi',3.232,[32,32,'stu'],(3,4))
+#name = ('bibi',3.232,[32,32,'stu'],(3,4))
 # names =[i + '\n' for i in name]
 # print(names)
 # for i in name:
 #     print(i)
 
-emp = {'name':'tom','salary':4324,'age':49}
-for k,v in emp.items():
-    print(k, v)
+# emp = {'name':'tom','salary':4324,'age':49}
+# for k,v in emp.items():
+#     print(k, v)
+
+
+#同时遍历序列和打印下标
+for i,s in enumerate(name):
+    print(i,s)
+
+# for i in range(len(name)):
+#     print(i,name[i])
+
+# i = 0
+# for x in name:
+#     print(i,x)
+#     i += 1
